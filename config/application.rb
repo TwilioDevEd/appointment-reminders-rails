7e2823d87b3689d6891ae09b83db5673f866a52f
Dvitto <patillacode@gmail.com>
2019-01-22 13:58:47 +0100

    Removed 'new' word (fresh app). Testing travis ruby version.

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module AppointmentRemindersRailsNew
+module AppointmentRemindersRails
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
@@ -15,7 +15,7 @@ module AppointmentRemindersRailsNew
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
- 
+
     config.active_job.queue_adapter = :delayed_job
   end
 end
